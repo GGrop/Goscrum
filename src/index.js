743f8359ca0eb8b50d27873c4806905aa48c6803
GGrop <giuliano.gropponi@gmail.com>
2022-08-03 16:11:25 -0300

    add hashRouter

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {store} from './store/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="Goscrum">
+    <HashRouter>
     <Provider store={store}>
       <App/>
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
