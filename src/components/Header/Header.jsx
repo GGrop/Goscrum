04904eff611b98d97e350af7521eea08faf159ae
GGrop <giuliano.gropponi@gmail.com>
2022-10-04 09:19:52 -0300

    header fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -21,9 +21,9 @@ export const Header = () => {
 
     return (
         <header>
-            <div>
-                <img src="https://imgur.com/KPXR8Tg" alt="" />
-                <h4 className="pointer" onClick={()=>navigate("/", {replace:true})}>Go Scrum</h4>
+            <div onClick={()=>navigate("/", {replace:true})}>
+                <img src="https://i.imgur.com/KPXR8Tg.png" alt="" />
+                <h4 className="pointer">Go Scrum</h4>
             </div>
             <div className="wrapper_right_header">
                 <button className="donar" onClick={()=>navigate("/donate", {replace:true})}>Donar!</button>
